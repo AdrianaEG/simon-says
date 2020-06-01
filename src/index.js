9450a1f7d860263071d8f8318820c4172c335403
AdrianaEG <adriana.ngc438@gmail.com>
2020-06-01 13:54:26 -0300

    obtener panel aleatorio, juega la computadora

--- src/index.js
+++ src/index.js
@@ -9,4 +9,26 @@ function ejecutarJuego(){
     juegoComputadora = [];
     jugarComputadora();
 }
+
+function jugarComputadora(){
+    let $turno = document.querySelector('.turno');
+    let numeroPanel = obtenerNumeroAleatorio();
+    console.log(numeroPanel);
+    juegoComputadora.push(`opcion-${numeroPanel}`);
+    console.log("Juego de computadora tiene " + juegoComputadora); 
+    $turno.textContent = "Jugando computadora..."; 
+    marcarTablero(juegoComputadora);
+}
+
+function marcarTablero(juego){
+    console.log("entra a marcar tablero");
+    for(let i=0; i<juego.length; i++){
+        //document.querySelector(`.${juego[i]}`).innerHTML = "HOLA";
+    }
+}
+
+function obtenerNumeroAleatorio(){
+    return(parseInt(Math.random()*(5-1)+1));
+}
+
 $botonComenzar.addEventListener('click', ejecutarJuego);
