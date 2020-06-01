99e4e4b29f92f74346f3b93556bef0070a30f249
AdrianaEG <adriana.ngc438@gmail.com>
2020-06-01 17:29:06 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const $botonComenzar = document.querySelector('#btn-comenzar');
 let juegoComputadora;
+let $turno = document.querySelector('.turno');
 /*$botonComenzar.onclick = function(e){
     e.preventDefault();
     console.log("click");
@@ -8,23 +9,48 @@ let juegoComputadora;
 function ejecutarJuego(){
     juegoComputadora = [];
     jugarComputadora();
+    jugarUsuario();
 }
 
 function jugarComputadora(){
-    let $turno = document.querySelector('.turno');
+    $turno.textContent = "Jugando computadora...";
     let numeroPanel = obtenerNumeroAleatorio();
-    console.log(numeroPanel);
     juegoComputadora.push(`opcion-${numeroPanel}`);
-    console.log("Juego de computadora tiene " + juegoComputadora); 
-    $turno.textContent = "Jugando computadora..."; 
+    console.log('La jugada de la compu es ' + juegoComputadora);
     marcarTablero(juegoComputadora);
 }
 
+function jugarUsuario(){
+    
+}
+
 function marcarTablero(juego){
-    console.log("entra a marcar tablero");
     for(let i=0; i<juego.length; i++){
-        //document.querySelector(`.${juego[i]}`).innerHTML = "HOLA";
+        let panelAMarcar = document.querySelector(`.${juego[i]}`);
+        let sonidos = ['audio-1', 'audio-2', 'audio-3', 'audio-4'];
+        if(juego[i] === "opcion-1"){
+            colorear("#F2388F", "#FEBCB9", panelAMarcar, sonidos[0]);
+        }
+        else if(juego[i] === "opcion-2"){
+            colorear("#049DD9", "#8DE7F5", panelAMarcar,sonidos[1]);
+        }
+        else if(juego[i] === "opcion-3"){
+            colorear("#65E01D", "#C9FF85", panelAMarcar, sonidos[2]);
+        }
+        else{
+            colorear("#FFE110", "#FFF9A3", panelAMarcar, sonidos[3]);
+        }
     }
+}
+
+function colorear(colorNuevo, colorAnterior, elemento, sonido){
+    setTimeout(function(){ 
+        elemento.style.background = colorNuevo;
+        document.querySelector(`.${sonido}`).play();
+    }, 500);
+    setTimeout(function(){
+        elemento.style.background = colorAnterior;
+    }, 1200);
 }
 
 function obtenerNumeroAleatorio(){
